ecdc2e3f0e111b7bc75dfe705433971b6c651234
Artem Shoobovych <shybovycha@gmail.com>
2015-01-01 12:19:11 +0100

    Done addition

--- task8/BigInteger.cs
+++ task8/BigInteger.cs
@@ -4,106 +4,144 @@ using System.Linq;
 
 namespace Exercise
 {
-	public class BigInteger
-	{
-		public List<int> Digits = new List<int>();
-		public Boolean Negative = false;
+    public class BigInteger
+    {
+        public List<int> Digits = new List<int>();
+        public Boolean Negative = false;
 
-		public BigInteger()
-		{
-		}
+        public BigInteger()
+        {
+        }
 
-		public BigInteger(String value)
-		{
-			if (value[0] == '-') {
-				Negative = true;
-				value = value.Substring(1);
-			}
+        public BigInteger(String value)
+        {
+            if (value[0] == '-') {
+                Negative = true;
+                value = value.Substring(1);
+            }
 				
-			for (int i = value.Length - 1; i > -1; i--) {
-				Digits.Add(int.Parse(value[i].ToString()));
-			}
-		}
+            for (int i = value.Length - 1; i > -1; i--) {
+                Digits.Add(int.Parse(value[i].ToString()));
+            }
+        }
 
-		public override String ToString()
-		{
-			String result = "";
+        public override String ToString()
+        {
+            String result = "";
 
-			if (Negative) {
-				result += "-";
-			}
+            if (Negative) {
+                result += "-";
+            }
 
-			for (int i = Digits.Count - 1; i > -1; i--) {
-				result += Digits[i].ToString();
-			}
+            for (int i = Digits.Count - 1; i > -1; i--) {
+                result += Digits[i].ToString();
+            }
 
-			return result;
-		}
+            return result;
+        }
 
-		public static BigInteger operator -(BigInteger value)
-		{
-			BigInteger result = new BigInteger();
+        public static BigInteger operator -(BigInteger value)
+        {
+            BigInteger result = new BigInteger();
 
-			result.Digits.AddRange(value.Digits);
-			result.Negative = !value.Negative;
+            result.Digits.AddRange(value.Digits);
+            result.Negative = !value.Negative;
 
-			return result;
-		}
+            return result;
+        }
 
-		public static BigInteger operator +(BigInteger a, BigInteger b)
-		{
-			BigInteger result = new BigInteger();
+        public static BigInteger operator +(BigInteger a, BigInteger b)
+        {
+            BigInteger result = new BigInteger();
 
-			return result;
-		}
+            int mem = 0;
 
-		public static BigInteger operator -(BigInteger a, BigInteger b)
-		{
-			BigInteger result = new BigInteger();
+            List<int> v1 = new List<int>();
+            List<int> v2 = new List<int>();
+            List<int> v_res = new List<int>();
 
-			return result;
-		}
+            v1.AddRange(a.Digits);
+            v2.AddRange(b.Digits);
 
-		public static BigInteger operator *(BigInteger a, BigInteger b)
-		{
-			BigInteger result = new BigInteger();
+            int max_length = Math.Max(v1.Count(), v2.Count());
 
-			return result;
-		}
+            for (int i = 0; i < max_length; i++) {
+                int tmp = mem;
 
-		public static BigInteger operator /(BigInteger a, BigInteger b)
-		{
-			BigInteger result = new BigInteger();
+                if (i < v1.Count())
+                    tmp += v1[i];
 
-			return result;
-		}
+                if (i < v2.Count())
+                    tmp += v2[i];
 
-		public static BigInteger operator %(BigInteger a, BigInteger b)
-		{
-			BigInteger result = new BigInteger();
+                int digit = tmp % 10;
 
-			result = a - ((a / b) * b);
+                mem = 0;
 
-			return result;
-		}
+                if (tmp >= 10) {
+                    mem = tmp / 10;
+                }
 
-		public static bool operator ==(BigInteger a, BigInteger b)
-		{
-			return (a.Negative == b.Negative) && (Enumerable.SequenceEqual(a.Digits.OrderBy(t => t), b.Digits.OrderBy(t => t)));
-		}
+                v_res.Add(digit);
+            }
 
-		public static bool operator !=(BigInteger a, BigInteger b)
-		{
-			return !(a == b);
-		}
+            while (mem > 9) {
+                v_res.Add(mem % 10);
+                mem /= 10;
+            }
 
-		public override bool Equals(object other)
-		{
-			bool a = this.GetType() == other.GetType(),
-			b = this.Negative == ((BigInteger) other).Negative,
-			c = Enumerable.SequenceEqual(this.Digits.OrderBy(t => t), ((BigInteger) other).Digits.OrderBy(t => t));
+            result.Digits.AddRange(v_res);
 
-			return ((a) && (b) && (c));
-		}
-	}
+            return result;
+        }
+
+        public static BigInteger operator -(BigInteger a, BigInteger b)
+        {
+            BigInteger result = new BigInteger();
+
+            return result;
+        }
+
+        public static BigInteger operator *(BigInteger a, BigInteger b)
+        {
+            BigInteger result = new BigInteger();
+
+            return result;
+        }
+
+        public static BigInteger operator /(BigInteger a, BigInteger b)
+        {
+            BigInteger result = new BigInteger();
+
+            return result;
+        }
+
+        public static BigInteger operator %(BigInteger a, BigInteger b)
+        {
+            BigInteger result = new BigInteger();
+
+            result = a - ((a / b) * b);
+
+            return result;
+        }
+
+        public static bool operator ==(BigInteger a, BigInteger b)
+        {
+            return (a.Negative == b.Negative) && (Enumerable.SequenceEqual(a.Digits.OrderBy(t => t), b.Digits.OrderBy(t => t)));
+        }
+
+        public static bool operator !=(BigInteger a, BigInteger b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object other)
+        {
+            bool a = this.GetType() == other.GetType(),
+            b = this.Negative == ((BigInteger) other).Negative,
+            c = Enumerable.SequenceEqual(this.Digits.OrderBy(t => t), ((BigInteger) other).Digits.OrderBy(t => t));
+
+            return ((a) && (b) && (c));
+        }
+    }
 }
